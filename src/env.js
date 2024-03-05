5c0a18538553e256a6c2dcc975d2f855cd3667d8
Matt McCoy <mattcmccoy01@gmail.com>
2024-03-05 14:35:57 -0500

    env cleanup

--- src/env.js
+++ src/env.js
@@ -33,11 +33,7 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
     WATCHMODE_API_KEY: z.string(),
-    WATCHMODE_API_URL: z.string().url(),
-    TMDB_API_READ_ACCESS_TOKEN: z.string(),
-    TMDB_API_KEY: z.string(),
-    TMDB_API_URL: z.string().url(),
-    TMDB_API_IMAGE_URL: z.string().url()
+    TMDB_API_KEY: z.string()
   },
 
   /**
@@ -63,11 +59,7 @@ export const env = createEnv({
     WATCHMODE_API_KEY: process.env.WATCHMODE_API_KEY,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    WATCHMODE_API_URL: process.env.WATCHMODE_API_URL,
-    TMDB_API_READ_ACCESS_TOKEN: process.env.TMDB_API_READ_ACCESS_TOKEN,
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
-    TMDB_API_URL: process.env.TMDB_API_URL,
-    TMDB_API_IMAGE_URL: process.env.TMDB_API_IMAGE_URL
+    TMDB_API_KEY: process.env.TMDB_API_KEY
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
