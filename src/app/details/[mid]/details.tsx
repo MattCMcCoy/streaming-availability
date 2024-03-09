cc58f71db7acadf6e7c5b75f832563f2aa511727
Matt McCoy <mattcmccoy01@gmail.com>
2024-03-09 08:28:14 -0500

    comment section

--- src/app/details/[mid]/details.tsx
+++ src/app/details/[mid]/details.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import { type Session } from 'next-auth';
+
 import { api } from '~/trpc/react';
 
+import { CommentSection } from './CommentSection';
 import { ServerImage } from './ServerImage';
 
-export default function MovieDetails({ mid }: { mid: number }) {
+export default function MovieDetails({
+  mid,
+  session
+}: {
+  mid: number;
+  session: Session | null;
+}) {
   const movieDetails = api.tmdb.details.useQuery(mid);
 
   if (movieDetails.isLoading) {
@@ -20,33 +29,29 @@ export default function MovieDetails({ mid }: { mid: number }) {
   }
 
   return (
-    <>
-      <div className="flex h-[90vh] flex-col lg:h-fit lg:flex-row">
-        <div className="flex h-fit w-fit flex-col items-center rounded border-streaminggold p-10 lg:ml-auto lg:flex-row lg:border lg:bg-streamingpurple/10">
-          <div className="relative my-auto mr-5 h-[30vh] w-[75vw] rounded-lg lg:w-[20vw] lg:bg-slate-700">
-            <ServerImage
-              src={
-                movieDetails.data.poster_path ??
-                movieDetails.data.backdrop_path ??
-                ''
-              }
-            />
+    <div className="flex flex-col lg:h-fit lg:flex-row">
+      <div className="flex h-fit w-fit flex-col items-center rounded border-streaminggold p-10 lg:ml-auto lg:flex-row lg:border lg:bg-streamingpurple/10">
+        <div className="relative my-auto mr-5 h-[30vh] w-[75vw] rounded-lg lg:w-[20vw] lg:bg-slate-700">
+          <ServerImage
+            src={
+              movieDetails.data.poster_path ??
+              movieDetails.data.backdrop_path ??
+              ''
+            }
+          />
+        </div>
+        <div>
+          <div className="h-fit border-b border-streaminggold pt-10 lg:w-[20vw]">
+            <div className="text-3xl text-white">{movieDetails.data.title}</div>
           </div>
-          <div>
-            <div className="h-fit border-b border-streaminggold pt-10 lg:w-[20vw]">
-              <div className="text-3xl text-white">
-                {movieDetails.data.title}
-              </div>
-            </div>
-            <div className="h-fit pt-10 lg:w-[20vw]">
-              <div className="text-xl text-white">
-                {movieDetails.data.overview}
-              </div>
+          <div className="h-fit pt-10 lg:w-[20vw]">
+            <div className="text-xl text-white">
+              {movieDetails.data.overview}
             </div>
           </div>
         </div>
-        {/* <CommentSection movieId={movieDetails.data.id} /> */}
       </div>
-    </>
+      <CommentSection movieId={movieDetails.data.id} session={session} />
+    </div>
   );
 }
--- src/app/details/[mid]/page.tsx
+++ src/app/details/[mid]/page.tsx
@@ -2,6 +2,7 @@ import { type InferPagePropsType } from 'next-typesafe-url';
 import { withParamValidation } from 'next-typesafe-url/app/hoc';
 import { AuthShowcase } from '~/app/(auth)/AuthShowcase';
 import { Nav } from '~/app/Nav';
+import { getServerAuthSession } from '~/server/auth';
 
 import MovieDetails from './details';
 import { Route, type RouteType } from './routeType';
@@ -9,15 +10,18 @@ import { Route, type RouteType } from './routeType';
 type PageProps = InferPagePropsType<RouteType>;
 
 async function Page({ routeParams }: PageProps) {
+  const session = await getServerAuthSession();
   return (
-    <div className="flex flex-col">
+    <div className="flex h-[100vh] flex-col ">
       <div className="mt-3 flex w-[100vw] flex-row pr-5 align-middle">
         <div className="mr-auto">
           <Nav />
         </div>
         <AuthShowcase />
       </div>
-      <MovieDetails mid={routeParams.mid} />;
+      <div className="mt-5">
+        <MovieDetails mid={routeParams.mid} session={session} />;
+      </div>
     </div>
   );
 }
